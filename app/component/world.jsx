5aba30cfd6db9637db81b7192466c99e93e64141
lizhiyao <lizy1993@163.com>
2016-03-25 16:10:49 +0800

    update
    Signed-off-by: lizhiyao <lizy1993@163.com>

---
+++ app/component/world.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default class World extends React.Component {
+  render() {
+    return <h1>World</h1>;
+  }
+}
+
+
+
--- app/main.js
+++ app/main.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Hello from './component.jsx';
+import Hello from './component/hello.jsx';
+import World from './component/world.jsx';
 
+function main() {
+  ReactDOM.render(
+    <Hello />,
+    document.getElementById('hello')
+  );
+  
+  ReactDOM.render(
+    <World />,
+    document.getElementById('world')
+  );
+}
 
-ReactDOM.render(
-  <Hello />,
-  document.getElementById('hello')
-);
+main();
